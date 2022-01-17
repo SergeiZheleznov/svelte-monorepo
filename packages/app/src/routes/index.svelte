0a7b89a1336073fe3866c48d1396a0dd88d9c89f
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2022-01-17 22:11:33 +0100

    Update Text component; Add sample of ussage in app package

--- packages/app/src/routes/index.svelte
+++ packages/app/src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import { Text } from '@sk/ui';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Text dimmed>sample text</Text>
--- packages/ui/src/lib/components/Text/Text.svelte
+++ packages/ui/src/lib/components/Text/Text.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	export let variant = 'h1';
+	export let dimmed = false;
 </script>
 
-<span class={variant}>
+<span class={`${variant}${dimmed ? ' dimmed' : ''}`}>
 	<slot />
+	{#if dimmed}
+		[dimmed]
+	{/if}
 </span>
+
+<style>
+	.dimmed {
+		color: gray;
+	}
+</style>
